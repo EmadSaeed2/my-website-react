c734407cfe7182966764017af4f0696d82a8230c
Emad Saeed <eaf.saeed@gmail.com>
2023-02-07 15:46:20 +0000

    home components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Slide from './components/Slide/Slide';
+import Portfolio from './components/Portfolio/Portfolio';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Slide />
+      <Portfolio />
+      <About />
+      <Contact />
     </div>
   );
 }
